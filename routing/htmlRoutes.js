1cb4368b7a059574e9a1e84eedf0eedca74ac007
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-04-28 13:10:20 -0400

    update

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,39 +1,12 @@
 var path = require("path");
 
-// var characters = [
-//     {
-//       routeName: "yoda",
-//       name: "Yoda",
-//       role: "Jedi Master",
-//       age: 900,
-//       forcePoints: 2000
-//     },
-//     {
-//       routeName: "darthmaul",
-//       name: "Darth Maul",
-//       role: "Sith Lord",
-//       age: 200,
-//       forcePoints: 1200
-//     },
-//     {
-//       routeName: "obiwankenobi",
-//       name: "Obi Wan Kenobi",
-//       role: "Jedi Master",
-//       age: 55,
-//       forcePoints: 1350
-//     }
-//   ];
-  
 module.exports = function(app) {
 	
 	// Basic route that sends the user first to the AJAX Page
-	app.get("/", function(req, res) {
-		res.sendFile(path.join(__dirname, "home.html"));
-	  });
-	app.get("/home", function(req, res) {
-		res.sendFile(path.join(__dirname, "home.html"));
-	});	  
-	app.get("/survey", function(req, res) {
-		res.sendFile(path.join(__dirname, "survey.html"));
+	app.use("/", function(req, res) {
+		res.sendFile(path.join(__dirname + "/../public/home.html"));
 	});
-};
+	app.get( function(req, res) {
+		res.sendFile(path.join(__dirname + "/../public/survey.html"));
+	});
+}
--- server.js
+++ server.js
@@ -3,10 +3,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var htmlRoutes = require("/Users/ezdeharjaber/Documents/cwru/FriendFinder/routing/htmlRoutes.js");
-var apiRoutes = require("/Users/ezdeharjaber/Documents/cwru/FriendFinder/routing/apiRoutes.js");
-// var htmlRoutes = require("./routing/htmlRoutes.js")
-// var apiRoutes = require("./routing/apiroutes.js")
+// var htmlRoutes = require("/../routing/htmlRoutes.js")
+// var apiRoutes = require("/../routing/apiRoutes.js")
 
 // Sets up the Express App
 // =============================================================
@@ -14,10 +12,22 @@ var app = express();
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
+
+app.get("/", function (req, res) {
+  res.send('Hello World');
+})
 //process.env to start up envirnment for app
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(function (req, res) {
+  res.setHeader("Content-Type", "text/plain")
+  res.write("you posted:\n")
+  res.end(JSON.stringify(req.body, null, 2))
+});
+
+require("./app/routing/htmlRoutes.js")(app);
 // Routes
 // =============================================================
 htmlRoutes(app);
