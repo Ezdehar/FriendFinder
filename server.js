c1695a09075e6fb02ed945d2c1e92c7c4151ed68
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-04-28 13:46:28 -0400

    update

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var http = require("http");
 var path = require("path");
-// var htmlRoutes = require("/../routing/htmlRoutes.js")
-// var apiRoutes = require("/../routing/apiRoutes.js")
+var htmlRoutes = require("/Users/ezdeharjaber/Documents/cwru/FriendFinder/routing/htmlRoutes.js");
+var apiRoutes = require("/Users/ezdeharjaber/Documents/cwru/FriendFinder/routing/apiRoutes.js");
 
 // Sets up the Express App
 // =============================================================
+
 var app = express();
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -18,21 +20,20 @@ app.get("/", function (req, res) {
 })
 //process.env to start up envirnment for app
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+/// Routes
+// =============================================================
+htmlRoutes(app);
+apiRoutes(app);
+  
 app.use(function (req, res) {
   res.setHeader("Content-Type", "text/plain")
   res.write("you posted:\n")
   res.end(JSON.stringify(req.body, null, 2))
 });
 
-require("./app/routing/htmlRoutes.js")(app);
-// Routes
-// =============================================================
-htmlRoutes(app);
-apiRoutes(app);
-  
+
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
